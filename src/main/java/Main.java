13bf8fdc16e7df9b28d528343ef288df3fe50952
Dave <dave@Zipcoders-MacBook-Pro-99.local>
2023-06-30 15:17:22 -0400

    submit project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,23 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
 
-    public static void main(String[] args ){
+    public static <scanner> void main(String[] args ){
         System.out.println("what is your name");
+        Scanner in = new Scanner(System.in);
+        String username = in.nextLine();
+        String alice= "alice";
+        String String = "bob";
 
+
+        if(alice.equals(username)) {
+            System.out.println("access granted");
+
+        }
 
 
 
